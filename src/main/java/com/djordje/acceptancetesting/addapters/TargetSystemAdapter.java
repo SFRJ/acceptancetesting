e71133a286f9490eab887d8242f46b9763ff0395
Ocean Silverfloat <ocean@silverfloat.com>
2014-12-18 22:43:06 +0000

    some comments added in addapters

--- src/main/java/com/djordje/acceptancetesting/addapters/TargetSystemAdapter.java
+++ src/main/java/com/djordje/acceptancetesting/addapters/TargetSystemAdapter.java
@@ -6,6 +6,8 @@ public class TargetSystemAdapter implements TargetSystem {
 
     @Override
     public void receivesData(Integer expected) {
-
+        // This is production code that the acceptance test avoids dealing with
+        // For example, a database client code, logging system, or other downstream system
+        // could go here(let's say)
     }
 }
--- src/main/java/com/djordje/acceptancetesting/addapters/UpstreamSystemAdapter.java
+++ src/main/java/com/djordje/acceptancetesting/addapters/UpstreamSystemAdapter.java
@@ -8,6 +8,8 @@ public class UpstreamSystemAdapter implements UpstreamSystem {
 
     @Override
     public List<Integer> data() {
+        // This is production code that the acceptance test avoids dealing with
+        // For example, it could be a rest endpoint...
         return null;
     }
 }
